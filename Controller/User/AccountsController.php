e255191a572fd1fca53d15c52c35811e335f9f3f
Javi H. Gil <javihgil@gmail.com>
2019-06-26 22:59:24 +0200

    User accounts basic feature

---
+++ Controller/User/AccountsController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Softspring\AccountBundle\Controller\User;
+
+use Softspring\Account\Model\UserMultiAccountedInterface;
+use Softspring\ExtraBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+
+class AccountsController extends AbstractController
+{
+    public function list(): Response
+    {
+        $user = $this->getUser();
+
+        if (!$user instanceof UserMultiAccountedInterface) {
+            throw new \Exception('Invalid user class');
+        }
+
+        return $this->render('@SfsAccount/user/accounts/list.html.twig', [
+
+        ]);
+    }
+}
