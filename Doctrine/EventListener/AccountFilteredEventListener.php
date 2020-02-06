33b66ae5fa70fcbf5432a247041ef370a9034e1e
Javi H. Gil <javihgil@gmail.com>
2020-02-06 13:57:07 +0100

    Add AccountFiltered event listener to assign account automatically on persist

---
+++ Doctrine/EventListener/AccountFilteredEventListener.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Softspring\AccountBundle\Doctrine\EventListener;
+
+use Doctrine\Common\EventSubscriber;
+use Doctrine\Common\Persistence\Event\LifecycleEventArgs;
+use Doctrine\ORM\Events;
+use Softspring\AccountBundle\Model\AccountFilterInterface;
+use Softspring\AccountBundle\Model\AccountInterface;
+use Symfony\Component\HttpFoundation\RequestStack;
+
+class AccountFilteredEventListener implements EventSubscriber
+{
+    /**
+     * @var RequestStack
+     */
+    protected $requestStack;
+
+    /**
+     * AccountFilteredEventListener constructor.
+     *
+     * @param RequestStack $requestStack
+     */
+    public function __construct(RequestStack $requestStack)
+    {
+        $this->requestStack = $requestStack;
+    }
+
+    public function getSubscribedEvents()
+    {
+        return [
+            Events::prePersist,
+        ];
+    }
+
+    public function prePersist(LifecycleEventArgs $eventArgs)
+    {
+        $entity = $eventArgs->getObject();
+
+        if (!$entity instanceof AccountFilterInterface) {
+            return;
+        }
+
+        if ($entity->getAccount()) {
+            return;
+        }
+
+        $entity->setAccount($this->getAccount());
+    }
+
+    /**
+     * @return AccountInterface|null
+     */
+    private function getAccount(): ?AccountInterface
+    {
+        if (!$request = $this->requestStack->getCurrentRequest()) {
+            return null;
+        }
+
+        return $request->attributes->get('_account', null);
+    }
+}
