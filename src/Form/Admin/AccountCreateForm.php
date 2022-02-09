7f8c2b3b1397a140e26c0082e75eb862262d9372
Javi H. Gil <javihgil@softspring.es>
2022-02-09 18:47:34 +0100

    Show user data

--- src/Form/Admin/AccountCreateForm.php
+++ src/Form/Admin/AccountCreateForm.php
@@ -3,6 +3,9 @@
 namespace Softspring\AccountBundle\Form\Admin;
 
 use Softspring\AccountBundle\Model\AccountInterface;
+use Softspring\UserBundle\Model\NameSurnameInterface;
+use Softspring\UserBundle\Model\UserInterface;
+use Softspring\UserBundle\Model\UserWithEmailInterface;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -21,6 +24,18 @@ class AccountCreateForm extends AbstractType implements AccountCreateFormInterfa
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder->add('name');
-        $builder->add('owner');
+        $builder->add('owner', null, [
+            'choice_label' => function (UserInterface $owner) {
+                if ($owner instanceof NameSurnameInterface) {
+                    return sprintf('%s %s', $owner->getName(), $owner->getSurname());
+                }
+
+                if ($owner instanceof UserWithEmailInterface) {
+                    return $owner->getEmail();
+                }
+
+                return $owner->getUserIdentifier();
+            },
+        ]);
     }
 }
