24a8669282ce55deb723aa65f3513bf195d5b65a
Javi H. Gil <javihgil@softspring.es>
2022-03-29 15:35:49 +0200

    Improve fixtures

--- src/DataFixtures/AccountFixtures.php
+++ src/DataFixtures/AccountFixtures.php
@@ -3,6 +3,7 @@
 namespace Softspring\AccountBundle\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Bundle\FixturesBundle\FixtureGroupInterface;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
@@ -12,7 +13,7 @@ use Softspring\UserBundle\DataFixtures\UserFixtures;
 use Softspring\UserBundle\Model\OwnerInterface;
 use Softspring\UserBundle\Model\UserInterface;
 
-class AccountFixtures extends Fixture implements DependentFixtureInterface
+class AccountFixtures extends Fixture implements DependentFixtureInterface, FixtureGroupInterface
 {
     protected AccountManagerInterface $accountManager;
 
@@ -26,7 +27,7 @@ class AccountFixtures extends Fixture implements DependentFixtureInterface
         $users = $manager->getRepository(UserInterface::class)->findAll();
 
         for ($i=0 ; $i < 300 ; $i++) {
-            $account = $this->createAccount();
+            $account = $this->createAccount($manager);
 
             if ($account instanceof OwnerInterface) {
                 $account->setOwner($users[array_rand($users)]);
@@ -38,7 +39,7 @@ class AccountFixtures extends Fixture implements DependentFixtureInterface
         $manager->flush();
     }
 
-    protected function createAccount(): AccountInterface
+    protected function createAccount(ObjectManager $manager): AccountInterface
     {
         $faker = \Faker\Factory::create('es_ES');
 
@@ -54,5 +55,10 @@ class AccountFixtures extends Fixture implements DependentFixtureInterface
         return [
             UserFixtures::class,
         ];
+    }
+
+    public static function getGroups(): array
+    {
+        return ['sfs_account'];
     }
 }
